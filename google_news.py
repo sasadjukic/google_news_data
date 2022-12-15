90e97941c0099821b5ce799282ac7bda1239d064
Sasa Djukic <djukicmsasa@gmail.com>
2022-12-15 10:31:48 +0100

    updated code to match google changes

--- google_news.py
+++ google_news.py
@@ -22,7 +22,7 @@ class DataBase:
         self.mydb = mysql.connector.connect(
             host = 'localhost',
             user = 'root',
-            password = 'Beograd91&',
+            password = 'password',
             database = 'GoogleNews'
             )
 
